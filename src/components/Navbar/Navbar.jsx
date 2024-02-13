3a214df34b98ed8e8118fa774367ba8ce899187d
Rohit Singh <rohit.s@antino@antino.com>
2024-02-13 09:42:10 +0530

    feat:added logo in the header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,21 @@
-import React, { useContext, useState } from 'react';
 import styles from './Navbar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faHeart, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import {Logo} from '../Logo';
 
 export default function Navbar() {
-    const[dropdownVal,setDropdownVal]=useState("filterby");
-    const handleDropdownVal=(e)=>{
-        setDropdownVal(e.target.value);
-        console.log(e.target.value);
-    }
-  return (
-    <div className={styles.navBar}>
-      <div className={styles.searchBar}>
-        <input type="text" placeholder="Search..." />
-        <FontAwesomeIcon icon={faSearch} fontSize={`21px`} style={{marginLeft:`12px`}}/>
-      </div>
-      <div className={styles.navRight}>
-        <div className={styles.favoriteBadge}>
-          <FontAwesomeIcon icon={faHeart} />
-          <span className={styles.badge}>3</span>
+    return (
+        <div className={styles.navBar}>
+            <Logo />
+            <div className={styles.navRight}>
+                <div className={styles.favoriteBadge}>
+                    <FontAwesomeIcon icon={faHeart} />
+                    <span className={styles.badge}>3</span>
+                </div>
+                <div className={styles.searchBar}>
+                    <input type="text" placeholder="Search..." />
+                </div>
+            </div>
         </div>
-        <div className={styles.dropdown}>
-          <select className={styles.select} value={dropdownVal} onChange={handleDropdownVal}>
-          <option value="filterBy"  selected>Filter By</option>
-          <option value="Name">Name</option>
-        </select>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
