05a02c8eae2c6a8194adba6576b1a7753c490184
Rohit Singh <rohit.s@antino@antino.com>
2024-02-13 17:42:08 +0530

    fix:fixed problem of losing color of heart

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './PokemonCard.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addToFavorites, removeFromFavorites } from '@redux/slices/favorites.slice';
 
 
 export default function PokemonCard({ pokemon }) {
     const dispatch = useDispatch();
     const [isFavorite, setIsFavorite] = useState(false);
+    const favorites = useSelector(state => state.favorites.favorites);
+
+    useEffect(() => {
+        setIsFavorite(favorites.some(favorite => favorite.id === pokemon.id));
+    }, [favorites, pokemon]);
+
     const toggleFavorite = () => {
         if (!isFavorite) {
             dispatch(addToFavorites(pokemon));
